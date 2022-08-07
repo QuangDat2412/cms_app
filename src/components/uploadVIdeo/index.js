3580b0287a36f367eab38af2f674dbaa9468d995
QuangDat2412 <QuangDat2412@github.com>
2022-08-07 14:18:05 +0700

    add upload progress

--- src/components/uploadVIdeo/index.js
+++ src/components/uploadVIdeo/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './index.scss';
-import { uploadImage } from 'src/apis/options';
+// import { uploadImage } from 'src/apis/options';
 import { Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
+import { OthersAction } from 'src/redux/others/slice';
+import { useDispatch } from 'react-redux';
+import { userRequest } from 'src/services/axiosServices';
 
 const UploadVideo = ({ setInfo, type, url }) => {
+    const dispatch = useDispatch();
+    const [progress, setProgress] = useState(0);
     const _uploadImage = () => {
         let inputTag = document.createElement('input');
         inputTag.type = 'file';
@@ -22,18 +27,36 @@ const UploadVideo = ({ setInfo, type, url }) => {
             media.onloadedmetadata = function (e) {
                 duration = media.duration;
             };
-            uploadImage(formData)
+            userRequest
+                .post('/upload', formData, {
+                    onUploadProgress: (progressEvent) => {
+                        // let downloadCount = DownloadCount(progressEvent.timeStamp, progressEvent.total, progressEvent.loaded);
+                        let percentCompleted = Math.floor((progressEvent.loaded * 100) / progressEvent.total);
+                        setProgress(percentCompleted);
+                    },
+                })
                 .then((res) => {
                     setInfo({ url: res.data, duration: Math.floor(duration) });
+                    dispatch(OthersAction.showToasrt({ type: 'success', message: 'Upload dữ liệu thành công' }));
                 })
-                .catch(() => {});
+                .catch((error) => {
+                    dispatch(OthersAction.showToasrt({ type: 'error', message: 'Upload dữ liệu không thành công' }));
+                });
         };
         inputTag.click();
     };
     return (
         <>
-            <Button onClick={_uploadImage} className="btn-outline-info" icon={<UploadOutlined />}>
-                Upload
+            <Button
+                onClick={_uploadImage}
+                className="btn-outline-info"
+                icon={<UploadOutlined />}
+                style={{ position: 'relative', color: '#000000d9' }}
+                type="primary"
+                ghost
+            >
+                <div style={{ position: 'absolute', height: '10%', width: progress + '%', bottom: 0, left: 0, backgroundColor: '#1890ff' }}></div>
+                <span style={{ marginLeft: '10px' }}>Upload</span>
             </Button>
         </>
     );
